5de38bf8002b6a851cbd7f20c196fd9d054b3dbe
LucasBarbosaLadeia <161781769+LucasBarbosaLadeia@users.noreply.github.com>
2024-11-11 21:51:34 -0300

    Add files via upload
    finalização do item

--- techAcademy/src/main/java/projeto/techAcademy/model/Item.java
+++ techAcademy/src/main/java/projeto/techAcademy/model/Item.java
@@ -1,4 +1,61 @@
 package projeto.techAcademy.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+@Table
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_item")
+    private Integer idItem;
+    @Column(name = "nome_item")
+    private String nomeItem;
+    @Column(name = "descricao_item")
+    private String descricaoItem;
+    @Column(name = "quantidade_item")
+    private Integer quantidadeItem;
+    @Column
+    private BigDecimal preco;
+
+    public Integer getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(Integer idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getNomeItem() {
+        return nomeItem;
+    }
+
+    public void setNomeItem(String nomeItem) {
+        this.nomeItem = nomeItem;
+    }
+
+    public String getDescricaoItem() {
+        return descricaoItem;
+    }
+
+    public void setDescricaoItem(String descricaoItem) {
+        this.descricaoItem = descricaoItem;
+    }
+
+    public Integer getQuantidadeItem() {
+        return quantidadeItem;
+    }
+
+    public void setQuantidadeItem(Integer quantidadeItem) {
+        this.quantidadeItem = quantidadeItem;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
--- techAcademy/src/main/java/projeto/techAcademy/repository/ItemRepository.java
+++ techAcademy/src/main/java/projeto/techAcademy/repository/ItemRepository.java
@@ -1,6 +1,7 @@
 package projeto.techAcademy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import projeto.techAcademy.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
